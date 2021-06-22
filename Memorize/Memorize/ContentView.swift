7c9e503b11051599ff42f81581950c8a6c7c7a02
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2021-06-22 14:14:04 +0300

    Initial project

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Danylo Sluzhynskyi on 22.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Danylo!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
